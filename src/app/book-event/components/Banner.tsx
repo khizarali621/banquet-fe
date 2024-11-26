bb544cffac37bc5d85ae07f0b553630f96afa33e
Zaid Ali <zaidalisp786@gmail.com>
2024-11-26 17:52:22 +0500

    changings

--- src/app/book-event/components/Banner.tsx
+++ src/app/book-event/components/Banner.tsx
@@ -4,7 +4,7 @@ import { Container } from "@/components/common";
 
 const Banner = () => {
   return (
-    <div className="md:py-[174px] py-[120px] bg-[url('/assets/images/book-bg.png')] bg-cover relative z-20">
+    <div className="md:py-[174px] py-[121px] bg-[url('/assets/images/book-bg.png')] bg-cover relative z-20">
       <Container>
         <div className="">
           <h1 className="h1 text-center">Book your event</h1>
